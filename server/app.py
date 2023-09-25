b63e6abc35948a36319d21a54b53cfae5f5eb028
Emmanuel <emwanzia532@gmail.com>
2023-09-25 10:07:13 +0300

    Finished code challenge

---
+++ server/app.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request
+from flask_migrate import Migrate
+from flask_cors import CORS
+from config import db
+from model.hero import Hero
+from model.heroPower import HeroPower
+from model.power import Power
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app)
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return 'Heroes will save us for sure!!!'
+
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    heroes_list = [{"id": hero.id, "name": hero.name, "super_name": hero.super_name} for hero in heroes]
+    return jsonify(heroes_list)
+
+@app.route('/heroes/<int:id>', methods=['GET'])
+def get_hero(id):
+    hero = Hero.query.get(id)
+    if hero:
+        hero_data = {
+            "id": hero.id,
+            "name": hero.name,
+            "super_name": hero.super_name,
+            "powers": [{"id": power.id, "name": power.name, "description": power.description} for power in hero.powers]
+        }
+        return jsonify(hero_data)
+    else:
+        return jsonify({"error": "Hero not found"}), 404
+
+@app.route('/powers', methods=['GET'])
+def get_powers():
+    powers = Power.query.all()
+    powers_list = [{"id": power.id, "name": power.name, "description": power.description} for power in powers]
+    return jsonify(powers_list)
+
+@app.route('/powers/<int:id>', methods=['GET'])
+def get_power(id):
+    power = Power.query.get(id)
+    if power:
+        power_data = {"id": power.id, "name": power.name, "description": power.description}
+        return jsonify(power_data)
+    else:
+        return jsonify({"error": "Power not found"}), 404
+
+@app.route('/powers/<int:id>', methods=['PATCH'])
+def update_power(id):
+    power = Power.query.get(id)
+    if power:
+        data = request.get_json()
+        if 'description' in data:
+            power.description = data['description']
+            db.session.commit()
+            return jsonify({"id": power.id, "name": power.name, "description": power.description})
+        else:
+            return jsonify({"error": "Invalid request"}), 400
+    else:
+        return jsonify({"error": "Power not found"}), 404
+
+@app.route('/hero_powers', methods=['POST'])
+def create_hero_power():
+    data = request.get_json()
+    strength = data.get('strength')
+    power_id = data.get('power_id')
+    hero_id = data.get('hero_id')
+
+    if not strength or not power_id or not hero_id:
+        return jsonify({"error": "Invalid request"}), 400
+
+    if strength not in ['Strong', 'Weak', 'Average']:
+        return jsonify({"errors": ["Validation error: 'strength' must be one of 'Strong', 'Weak', 'Average'"]}), 400
+
+    hero = Hero.query.get(hero_id)
+    power = Power.query.get(power_id)
+
+    if not hero or not power:
+        return jsonify({"error": "Hero or Power not found"}), 404
+
+    hero_power = HeroPower(strength=strength, hero=hero, power=power)
+
+    db.session.add(hero_power)
+    db.session.commit()
+
+    hero_data = {
+        "id": hero.id,
+        "name": hero.name,
+        "super_name": hero.super_name,
+        "powers": [{"id": p.id, "name": p.name, "description": p.description} for p in hero.powers]
+    }
+
+    return jsonify(hero_data), 201
+
+if __name__ == '__main__':
+    app.run(port=5555)
---
+++ server/seed.py
@@ -0,0 +1,61 @@
+from random import randint
+from app import app, db
+from db.models import Hero, Power, HeroPower
+from sqlalchemy.exc import IntegrityError
+
+def seed():
+    with app.app_context():
+        db.create_all() 
+
+        print("🦸‍♀️ Seeding powers...")
+        powers_data = [
+            {"name": "super strength", "description": "gives the wielder super-human strengths"},
+            {"name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed"},
+            {"name": "super human senses", "description": "allows the wielder to use her senses at a super-human level"},
+            {"name": "elasticity", "description": "can stretch the human body to extreme lengths"}
+        ]
+
+        powers = [Power(**data) for data in powers_data]
+        db.session.add_all(powers)
+        db.session.commit()
+
+        print("🦸‍♀️ Seeding heroes...")
+        heroes_data = [
+            {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
+            {"name": "Doreen Green", "super_name": "Squirrel Girl"},
+            {"name": "Stacy Queen", "super_name": "Spider-Gwen"},
+            {"name": "Babuze Prior", "super_name": "Dr. Papuse"},
+            {"name": "Nigga Groo", "super_name": "De Nigga"},
+            {"name": "Kai Cenat", "super_name": "Woolant Kai"},
+            {"name": "I Show Speed", "super_name": "Speedy"},
+            {"name": "J J", "super_name": "KSI"},
+            {"name": "Side Men", "super_name": "Hit EM All"},
+            {"name": "Peter Parker", "super_name": "Spiderman"},
+            {"name": "Robert Olwon", "super_name": "Jalango"},
+            {"name": "Type Shit", "super_name": "Auto Correct"},
+            {"name": "Suck Onn", "super_name": "Dicks On"},
+        ]
+
+        heroes = [Hero(**data) for data in heroes_data]
+        db.session.add_all(heroes)
+        db.session.commit()
+
+        print("🦸‍♀️ Adding powers to heroes...")
+        strengths = ["Strong", "Weak", "Average"]
+
+        for hero in heroes:
+            for _ in range(1, 4):
+                power = Power.query.order_by(db.func.random()).first()
+                hero_power = HeroPower(hero=hero, power=power, strength=strengths[randint(0, 2)])
+                try:
+                    db.session.add(hero_power)
+                    db.session.commit()
+                except IntegrityError:
+                    db.session.rollback()
+
+        print("🦸‍♀️ Done seeding!")
+
+if __name__ == "__main__":
+    seed()
+    print("Database seeded successfully.")
+
